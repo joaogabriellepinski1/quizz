5592cdecee9fdeb82fc406d7ef7f66a17632808b
joaogabriellepinski1 <108412125+joaogabriellepinski1@users.noreply.github.com>
2024-09-04 15:50:19 -0300

    ddf

--- js/script.js
+++ js/script.js
@@ -28,5 +28,36 @@
 
  function mostraPergunta() {
     if (atual >= perguntas.length) {
+       mostraResultado();
+       return;
     }
- }
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativa.textContent = "";
+    mostraAlternativas.textContent();
+}
+
+function mostraAlternativas() {
+   for (const  alternativa of perguntaAtual.alternativaO) {
+      const botaoAlternativas = Document.creataElement("buttom");
+      botaoAlternativas.textContent = alternativa.texto;
+      botaoAlternativas.addEventListener("click"); () => respostaSelecionada(alternativa));
+      caixaAlternativas.appendChild(botaoAlternativas);
+   }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+   const afirmacoes = aleatorio (opcaoSelecionada.afirmacao);
+   historiaFinal += afirmacaos +" ";
+   if (opcaoSelecionada.proxima. !== undefined ) {
+       atual = opcaoSelecionada.proxima;
+   }  else {
+      mostraResultado();
+      return;
+   }
+   mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = 'Em 2049,${nome}'
+}
