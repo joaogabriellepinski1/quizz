530de81d483ea78956d416427f1f986806262672
joaogabriellepinski1 <108412125+joaogabriellepinski1@users.noreply.github.com>
2024-10-02 15:45:14 -0300

    b

--- js/perguntas.js
+++ js/perguntas.js
@@ -3,7 +3,7 @@ export const perguntas = [
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "o é assustador!",
+                texto: "Isso é assustador!",
                 afirmacao: [
                     "No início ficou com medo do que essa tecnologia pode fazer.",
                     "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
